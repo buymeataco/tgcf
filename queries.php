2955a66ed388d04347a3adc8194b0b7114b27eb4
Thomas Rowley <tom@buymeataco.com>
2016-04-14 08:53:53 -0500

    Adds search functionality to search page.

--- queries.php
+++ queries.php
@@ -13,10 +13,7 @@ case 'searchPageResults':
 			array_push($idArray, $user_id);
 		}
 	if (!EMPTY($idArray)) {
-	$idLookupString = implode(",", $idArray);
-	// echo "IDArray: ";
-	// print_r($idArray);
-	// echo "<br /><br />";
+	//$idLookupString = implode(",", $idArray);
 	$userMetaValues = [];
 	$userMetaKeys = [];
 	$combinedKeyValuePairs = [];
@@ -24,22 +21,22 @@ case 'searchPageResults':
 	foreach ($idArray as $value) {
 		$nestedMetaValues = [];
 		$nestedKeys = [];		
-		$query2 = "SELECT meta_value FROM metaTest WHERE meta_key IN ('first_name','last_name','city','employer','title') AND user_id = '$value'";
+		$query2 = "SELECT meta_value FROM metaTest WHERE meta_key IN ('first_name','last_name','employer','title') AND user_id = '$value'";
 		$result2 = mysqli_query($conn,$query2) or die ("<br />Could not execute second query.");	
-		$resultCountMetaValues = mysqli_num_rows($result2);
 			while ($row2 = mysqli_fetch_array($result2)) {
 				extract ($row2);
 					array_push($nestedMetaValues, $row2[0]);
-	} //foreach
+			} //while
+
 	//Get all associated meta keys for user info.
-			foreach ($nestedMetaValues as $value2) {
-				$query3 = "SELECT meta_key FROM metaTest WHERE user_id = '$value' AND meta_value = '$value2'";					
-				$result3 = mysqli_query($conn,$query3) or die ("<br />Could not execute third query.");
-					while ($row3 = mysqli_fetch_array($result3)) {
-						extract($row3);
-							array_push($nestedKeys, $row3[0]);
-					}
-			} //foreach
+		foreach ($nestedMetaValues as $value2) {
+			$query3 = "SELECT meta_key FROM metaTest WHERE user_id = '$value' AND meta_value = '$value2'";
+			$result3 = mysqli_query($conn,$query3) or die ("<br />Could not execute third query.");
+				while ($row3 = mysqli_fetch_array($result3)) {
+					extract($row3);
+					array_push($nestedKeys, $row3[0]);
+				} //while
+		} //foreach
 		array_push($userMetaValues, $nestedMetaValues);
 		array_push($userMetaKeys, $nestedKeys);
 	} //foreach			
@@ -49,29 +46,9 @@ case 'searchPageResults':
 				$combinedArrays = array_combine($values5, $value4);
 			}
 		array_push($combinedKeyValuePairs, $combinedArrays);	
-	}
+	}		
 
-		// echo "<pre>";
-		// 	print_r($userMetaValues);
-		// echo "</pre>";
-		// echo "<br />"; 
-		// echo "<pre>";
-		// 	print_r($userMetaKeys);
-		// echo "</pre>";
-		echo "<pre>";
-			print_r($combinedKeyValuePairs);
-		echo "</pre>";
-		echo "<br />"; 		
-
-		// echo "<pre>";
-		// 	print_r($userMetaValues);
-		// echo "</pre>";
-		// echo "<br />"; 
-		// echo "<pre>";
-		// 	print_r($userMetaKeys);
-		// echo "</pre>";
-
-	displaySearchResults($resultCountMetaValues, $userMetaValues);
+	displaySearchResults($combinedKeyValuePairs);
 	cleanup($result, $result2, $result3, $idArray, $userMetaValues, $userMetaKeys, $nestedKeys, $nestedMetaValues);
 	} else {
 		echo "<p class=\"resultCountIndexPage\">Your search returned no results.</p>";
@@ -84,9 +61,10 @@ case 'memberDetails':
 
 function cleanup($result, $result2, $result3, $idArray, $userMetaValues, $userMetaKeys, $nestedKeys, $nestedMetaValues) {
 	if (isset($idArray, $userMetaValues, $userMetaKeys, $nestedKeys, $nestedMetaValues)) {unset($idArray, $userMetaValues, $userMetaKeys, $nestedKeys, $nestedMetaValues); }
-	mysqli_free_result($result);
-	mysqli_free_result($result2);
-	mysqli_free_result($result3);	
+		mysqli_free_result($result);
+		mysqli_free_result($result2);
+		mysqli_free_result($result3);	
+	
 	if (isset($conn)) {mysqli_close($conn); };
 } // end cleanupScripts()
 
--- searchLookup.php
+++ searchLookup.php
@@ -2,34 +2,29 @@
 $whichQuery = 'searchPageResults';
 include('queries.php');
 
-function displaySearchResults($resultCountMetaValues, $userMetaValues) {
+function displaySearchResults($combinedKeyValuePairs) {
 	$counter = 1;
-	$resultCountAdjusted = $resultCountMetaValues/3; 
-	echo "<p class=\"resultCountIndexPage\">Your search returned {$resultCountAdjusted} result(s).</p>";
-			$arrayLength = count($userMetaValues, COUNT_RECURSIVE);
-			for ($i=0; $i<=3;$i++) {
-					// $returnedValue1 = $searchData[$i++]; //first_name
-					// $returnedValue2 = $searchData[$i++]; //last_name
-					// $returnedValue3 = $searchData[$i]; //city
+	$i=0;
+	$arrayLength = count((array_keys($combinedKeyValuePairs)), COUNT_RECURSIVE);
+	// echo "<pre>";
+	// 	print_r($combinedKeyValuePairs);
+	// echo "</pre>";
+	echo "<p class=\"resultCountIndexPage\">Your search returned {$arrayLength} result(s).</p>";
+			while ($i < $arrayLength) {			
+					$myFirstName = $combinedKeyValuePairs[$i]["first_name"];
+					$myLastName = $combinedKeyValuePairs[$i]["last_name"];
+					$myTitle = $combinedKeyValuePairs[$i]["title"];
+					$myEmployer = $combinedKeyValuePairs[$i]["employer"];
 			 		$counter++;
 					$rowColor = ($counter & 1) ? $rowColor = 'resultDCDCDC' : $rowColor = 'resultC8DAE8';
 					echo "<a href=\"memberDetails.php?id=\"><div class=\"{$rowColor} cf\">
 							<ul class=\"resultListLoop\">
-								<li class=\"listResultName\"><a href=\"#\"></li>
-								<li class=\"listResultTitle\"><a href=\"#\"></li>
+								<li class=\"listResultName\"><a href=\"#\">{$myFirstName} {$myLastName}</li>
+								<li class=\"listResultTitle\"><a href=\"#\">{$myTitle}</li>
+								<li class=\"listResultTitle\"><a href=\"#\">{$myEmployer}</li>
 							</ul>
 					 	  </div></a>";
-					 	  //echo $i;
+					$i++;
 			}
-} // displaySearchResults()
-
-// echo "<pre>";
-// 	print_r($idArray);
-// echo "</pre>";
-// echo "<pre>";
-// 	print_r($searchData);
-// echo "</pre>";
-
-//echo "ID lookup string from php page: " . $idLookupString;
-
+} // end displaySearchResults()
 ?>
